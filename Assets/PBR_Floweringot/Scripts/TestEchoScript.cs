a219c7d127e77f588c8e455990e5c13045b58ac3
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2023-05-08 14:26:11 +0800

    Added a mainScreen

---
+++ Assets/PBR_Floweringot/Scripts/TestEchoScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEchoScript : MonoBehaviour
+{
+    public void clicked() {
+        Debug.Log("HI");
+    }
+}
