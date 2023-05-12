bd2c7db7bd4843e5d1f83b67664d26d665939afd
Brian <111887232+talon-2@users.noreply.github.com>
2023-05-12 23:23:06 +0800

    Created Shop menu

--- Assets/Scripts/InGameUIController.cs
+++ Assets/Scripts/InGameUIController.cs
@@ -11,38 +11,64 @@ public class InGameUIController : MonoBehaviour
     public GameObject waterButton;
     public GameObject shopButton;
 
-    public Animation hideShopAnim;
-    public Animation hideFertilizeAnim;
-    public Animation hideWaterAnim;
+    [Header("Animator")]
+    public Animator shopAnimator;
+    public Animator fertilizeAnimator;
+    public Animator waterAnimator;
+
+    public Animator shopMenuAnimator;
+
+    [Header("Panel")]
+    public GameObject shopPanel;
+    public GameObject inGamePanel;
 
     bool hideUI = false;
+    bool showShop = false;
 
     public void HideUIBtn()
     {
         if (!hideUI)
         {
-            hideWaterAnim.Play("HideWaterAnim");
-            hideShopAnim.Play("HideShopAnim");
-            hideFertilizeAnim.Play("HideFertilizeAnim");
-
+            shopAnimator.SetTrigger("HideUI");
+            fertilizeAnimator.SetTrigger("HideUI");
+            waterAnimator.SetTrigger("HideUI");
             hideUI = true;
         }
         else
         {
+            shopAnimator.SetTrigger("ShowUI");
+            fertilizeAnimator.SetTrigger("ShowUI");
+            waterAnimator.SetTrigger("ShowUI");
             hideUI = false;
         }
     }
+
     public void FertilizeBtn()
     {
 
     }
+
     public void WaterBtn()
     {
 
     }
+
     public void ShopBtn()
     {
-
+        if (showShop)
+        {
+            shopMenuAnimator.SetTrigger("CloseShop");
+            inGamePanel.SetActive(true);
+            shopPanel.SetActive(false);
+        }
+        else
+        {
+            shopMenuAnimator.SetTrigger("OpenShop");
+            inGamePanel.SetActive(false);
+            shopPanel.SetActive(true);
+        }
     }
+
+
 
 }
---
+++ Assets/Scripts/ShopMenuController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMenuController : MonoBehaviour
+{
+    [Header("Tab Buttons")]
+    public GameObject vegetablesBtn;
+    public GameObject decorativesBtn;
+    public GameObject herbsBtn;
+    public GameObject potsBtn;
+
+    [Header("Tabs")]
+    public GameObject vegeTab;
+    public GameObject decoTab;
+    public GameObject herbTab;
+    public GameObject potTab;
+
+    [Header("Vegetable Buy Buttons")]
+    public GameObject cornBtn;
+    public GameObject cucumberBtn;
+    public GameObject tomatoBtn;
+    public GameObject turnipBtn;
+    public GameObject cabbageBtn;
+
+    [Header("Decorative Buy Buttons")]
+    public GameObject sunflowerBtn;
+
+
+    [Header("Herb Buy Buttons")]
+    public GameObject cilantroBtn;
+
+    [Header("Pot Buy Buttons")]
+    public GameObject potBtn;
+
+    public void VegeTabBtn()
+    {
+
+    }
+    public void DecoTabBtn()
+    {
+
+    }
+    public void HerbTabBtn()
+    {
+
+    }
+    public void PotTabBtn()
+    {
+
+    }
+}
